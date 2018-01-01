ba48360766f8d8403e62c4e172617e71ea400847
amitcohen <amitcoh@bynet.co.il>
2018-01-02 01:51:01 +0200

    night check

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ TITLE = 'Amit-Test'
 @application.route("/")
 def hello():
     head = 'Hello!'
-    text = 'My name is Amit! I would like to upload this to OpenShift.'
+    text = 'My name is Amit! I would like to upload this to OpenShift. This is the night deployment test'
     return render_template('index.html', title=TITLE, head=head, text=text)
 
 if __name__ == "__main__":
