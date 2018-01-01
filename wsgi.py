640f587bbf3691c3e676a224c34521b75134d6c0
amitcohen <amitcoh@bynet.co.il>
2018-01-02 01:05:34 +0200

    init

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
+
+TITLE = 'Amit-Test'
 
 @application.route("/")
 def hello():
-    return "Hello World! I'm Amit"
+    head = 'Hello!'
+    text = 'My name is Amit!'
+    return render_template('index.html', title=TITLE, head=head, text=text)
 
 if __name__ == "__main__":
     application.run()
